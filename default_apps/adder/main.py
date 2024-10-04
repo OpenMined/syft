ec4efb17db38449e876434a218bee7d496a7956d
dk <dkn.work@protonmail.com>
2024-10-04 10:48:17 +0700

    replace os with pathlib for `default_apps/adder`

--- default_apps/adder/main.py
+++ default_apps/adder/main.py
@@ -1,28 +1,29 @@
 import json
 import os
+from pathlib import Path
 
 from syftbox.lib import ClientConfig
 
 config_path = os.environ.get("SYFTBOX_CLIENT_CONFIG_PATH", None)
 client_config = ClientConfig.load(config_path)
 
-input_folder = f"{client_config.sync_folder}/{client_config.email}/app_pipelines/adder/inputs/"
-output_folder = f"{client_config.sync_folder}/{client_config.email}/app_pipelines/adder/done/"
-os.makedirs(input_folder, exist_ok=True)
-os.makedirs(output_folder, exist_ok=True)
+input_folder = Path(client_config.sync_folder) / client_config.email / "app_pipelines" / "adder" / "inputs"
+output_folder = Path(client_config.sync_folder) / client_config.email / "app_pipelines" / "adder" / "done"
+input_folder.mkdir(parents=True, exist_ok=True)
+output_folder.mkdir(parents=True, exist_ok=True)
 
-input_file_path = f"{input_folder}data.json"
-output_file_path = f"{output_folder}data.json"
+input_file_path = input_folder / "data.json"
+output_file_path = output_folder / "data.json"
 
-if os.path.exists(input_file_path):
-    with open(input_file_path, "r") as f:
+if input_file_path.exists():
+    with input_file_path.open(mode="r") as f:
         data = json.load(f)
 
     data["datum"] += 1
 
-    with open(output_file_path, "w") as f:
+    with output_file_path.open(mode="w") as f:
         json.dump(data, f)
 
-    os.remove(input_file_path)
+    input_file_path.unlink()
 else:
     print(f"Input file {input_file_path} does not exist.")
