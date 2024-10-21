0b25b6fdfd79583f4ff94b524795f62b783da6e1
rasswanth-s <43314053+rasswanth-s@users.noreply.github.com>
2024-10-21 13:16:21 +0530

    remove redundant old code in ring_test.py

--- tests/integration/ring_test.py
+++ tests/integration/ring_test.py
@@ -43,98 +43,6 @@ def apps_pipeline_for(datasite_config: ClientConfig, app_name: str, state: str) 
     )
 
 
-# @pytest.mark.parametrize(
-#     "data_json",
-#     [
-#         (
-#             {
-#                 "ring": [
-#                     "alice@openmined.org",
-#                     "bob@openmined.org",
-#                     "alice@openmined.org",
-#                 ],
-#                 "data": 0,
-#                 "current_index": 0,
-#             },
-#             {
-#                 "ring": [
-#                     "alice@openmined.org",
-#                     "bob@openmined.org",
-#                     "alice@openmined.org",
-#                 ],
-#                 "data": 3,
-#                 "current_index": 2,
-#             },
-#         )
-#     ],
-# )
-# def test_syftbox_ring(
-#     monkeypatch: MonkeyPatch,
-#     datasite_1: ClientConfig,
-#     datasite_2: ClientConfig,
-#     data_json: tuple,
-# ):
-#     input_json, expected_json = data_json
-
-#     print(input_json, expected_json)
-
-#     datasite_1_shared_state = SharedState(client_config=datasite_1)
-#     datasite_2_shared_state = SharedState(client_config=datasite_2)
-
-#     print()
-#     install_ring(datasite_1, monkeypatch)
-#     install_ring(datasite_2, monkeypatch)
-#     run_apps(datasite_1_shared_state)
-#     run_apps(datasite_2_shared_state)
-#     sync_datasites([datasite_1, datasite_2])
-
-#     datasite_1_sync_folder = Path(datasite_1.sync_folder)
-#     print("datasite_1_sync_folder", datasite_1_sync_folder)
-#     datasite_2_sync_folder = Path(datasite_2.sync_folder)
-#     print("datasite_2_sync_folder", datasite_2_sync_folder)
-
-#     datasite_1_running_dir = apps_pipeline_for(datasite_1, "ring", "running")
-#     datasite_2_running_dir = apps_pipeline_for(datasite_2, "ring", "running")
-#     datasite_1_done_dir = apps_pipeline_for(datasite_1, "ring", "done")
-
-#     # Create data in alice's datasite
-#     assert (
-#         datasite_1_running_dir.exists()
-#     ), f"Running directory does not exist {datasite_1_running_dir}"
-
-#     with open(datasite_1_running_dir / "data.json", "w") as fp:
-#         json.dump(input_json, fp)
-
-#     # Run the ring app, sync the datasites. Data should be moved to bob's ring running dir
-#     run_apps(datasite_1_shared_state)
-#     sync_datasites([datasite_1, datasite_2])
-#     file_in_datasite_2 = datasite_2_running_dir / "data.json"
-#     assert file_in_datasite_2.exists(), f"File does not exist {file_in_datasite_2}"
-#     with file_in_datasite_2.open() as fp:
-#         data_in_datasite_2 = json.load(fp)
-#     assert data_in_datasite_2["data"] == 1
-#     assert data_in_datasite_2["current_index"] == 1
-
-#     # Run the ring app again, sync the datasites. Data should be moved to alice's ring running dir
-#     run_apps(datasite_2_shared_state)
-#     sync_datasites([datasite_1, datasite_2])
-#     file_in_datasite_1 = datasite_1_running_dir / "data.json"
-#     assert file_in_datasite_1.exists(), f"File does not exist {file_in_datasite_1}"
-#     with file_in_datasite_1.open() as fp:
-#         data_in_datasite_1 = json.load(fp)
-#     assert data_in_datasite_1["data"] == 2
-#     assert data_in_datasite_1["current_index"] == 2
-
-#     # Run the ring app again, sync the datasites. Data should be moved to alice's ring done dir
-#     run_apps(datasite_1_shared_state)
-#     sync_datasites([datasite_1, datasite_2])
-#     file_in_datasite_1 = datasite_1_done_dir / "data.json"
-#     assert file_in_datasite_1.exists(), f"File does not exist {file_in_datasite_1}"
-#     with file_in_datasite_1.open() as fp:
-#         data_in_datasite_1 = json.load(fp)
-#     assert data_in_datasite_1["data"] == expected_json["data"]
-#     assert data_in_datasite_1["current_index"] == expected_json["current_index"]
-
 #TODO: Solve name confusion in the server client before merge
 @pytest.mark.parametrize(
     "data_json",
@@ -210,7 +118,7 @@ def test_syftbox_ring_new(tmp_path: Path,server_client: TestClient, monkeypatch:
         assert data_in_next_datasite["data"] == idx+1
         assert data_in_next_datasite["current_index"] == idx+1
 
-    # Run the ring app again, sync the datasites. Data should be moved to alice's ring done dir
+    # Run the ring app again, sync the datasites. Data should be moved to last person's ring done dir
     run_apps(SharedState(client_config=datasites[-1]))
     sync_datasites(datasites)
     file_in_datasite_1 = datasite_1_done_dir / "data.json"
