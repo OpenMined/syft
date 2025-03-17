1bef660d5ace417a05cfb200cab7317b5eeb3c68
Tauquir <30658453+itstauq@users.noreply.github.com>
2025-03-17 23:22:04 +0530

    add support for serving CSS files in main router

--- syftbox/server/api/v1/main_router.py
+++ syftbox/server/api/v1/main_router.py
@@ -110,6 +110,8 @@ async def browse_datasite(
         if os.path.exists(slug_path) and os.path.isfile(slug_path):
             if slug_path.endswith(".html") or slug_path.endswith(".htm"):
                 return FileResponse(slug_path)
+            elif slug_path.endswith(".css"):
+                return FileResponse(slug_path, media_type="text/css")
             elif slug_path.endswith(".md"):
                 with open(slug_path, "r") as file:
                     content = file.read()
